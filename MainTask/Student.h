f0ca65dd931bec9a643c8e427756054870b587be
Danila Sosnov <dsosnov2009@gmail.com>
2025-04-08 17:44:13 +0300

    Add behaviour with method getString

--- MainTask/Student.h
+++ MainTask/Student.h
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Student {
 public:
+	//fields
 	string name;
 	int age;
 	double mark;
 	bool alive;
+
+	//methods
+	string getString()
+	{
+		string s = "Name: " + name;
+		s += ", age: " + to_string(age)
+			+ ", average mark: " + to_string(mark)
+			+ ", alive: " + (alive ? "yes" : "no");
+
+		return s;
+	}
 };
+
--- MainTask/main.cpp
+++ MainTask/main.cpp
@@ -12,10 +12,9 @@ int main() {
 	st2.age = 15;
 	st2.mark = 9.99;
 	st2.alive = false;
-
-	Student st = st1.mark > st2.mark ? st1 : st2;
-
-	cout << "Best student is: " << st.name << ".\n";
+	
+	cout << st1.getString() << endl;
+	cout << st2.getString() << endl;
 
 	return 0;
 }
