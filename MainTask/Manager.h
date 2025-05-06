e9f7bcc6b45e760deb03b4626caf83fadf29733b
Danila Sosnov <dsosnov2009@gmail.com>
2025-05-06 18:48:04 +0300

    Add Manager class

---
+++ MainTask/Manager.h
@@ -0,0 +1,13 @@
+#include "Initializer.h"
+
+
+class Manager
+{
+public:
+
+	Student* findBestStudents(Student* list, int count, int& bestCount);
+	Student* findWorstStudents(Student* list, int count, int& bestCount);
+
+
+};
+
--- MainTask/main.cpp
+++ MainTask/main.cpp
@@ -1,9 +1,18 @@
-#include "Initializer.h"
+#include "Manager.h"
 
+void showAllStudents(Student* list, int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		cout << list[i].toString() << endl;
+
+	}
+}
 int main() {
 	
 	Student* list = nullptr;
 	Initializer initializer;
+	Manager manager;
 
 	int count;
 
@@ -12,13 +21,21 @@ int main() {
 
 	initializer.init(list, count);
 
+	cout << "All students:\n";
+	showAllStudents(list, count);
+
+	int size = 0;
+	Student* bestList = manager.findBestStudents(list, count, size);
+	cout << "\nList of best students: ";
+	showAllStudents(bestList, size);
+
+	Student* worstList = manager.findWorstStudents(list, count, size);
+	cout << "\nList of worst students: ";
+	showAllStudents(worstList, size);
 
 
-	for (int i = 0; i < count; i++)
-	{
-		cout << list[i].toString() << endl;
 
-	}
+	
 
 	return 0;
 }
